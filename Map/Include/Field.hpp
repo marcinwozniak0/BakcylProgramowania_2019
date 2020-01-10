2ed389311e9029ff254e7bf59ca33e3c157936cb
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-01-10 13:31:53 +0100

    Add Field neighbours interface

--- Map/Include/Field.hpp
+++ Map/Include/Field.hpp
@@ -1,8 +1,22 @@
 #pragma once
+#include <memory>
+
+class Field;
+
+struct Neighbours
+{
+    std::shared_ptr<Field> up;
+    std::shared_ptr<Field> down;
+    std::shared_ptr<Field> left;
+    std::shared_ptr<Field> right;
+};
 
 class Field
 {
 public:
     virtual void action() = 0;
     virtual char getType() const = 0;
+protected:
+    Neighbours _neighbours;
+    virtual Neighbours& getNeighbours() {return _neighbours;}
 };
