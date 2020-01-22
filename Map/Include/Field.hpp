d08d57dd69a457aa690d1beb3a8ead00364e45e3
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-01-22 20:51:08 +0100

    Remove Neighbours struct

--- Map/Include/Field.hpp
+++ Map/Include/Field.hpp
@@ -2,16 +2,6 @@
 #include "FieldTypes.hpp"
 #include <memory>
 
-class Field;
-
-struct Neighbours
-{
-    std::shared_ptr<Field> up;
-    std::shared_ptr<Field> down;
-    std::shared_ptr<Field> left;
-    std::shared_ptr<Field> right;
-};
-
 class Field
 {
 public:
@@ -21,10 +11,6 @@ public:
 
     virtual void action() = 0;
     virtual FieldType getType() const = 0;
-
-protected:
-    Neighbours _neighbours;
-    virtual Neighbours& getNeighbours() {return _neighbours;}
 
 private:
     bool _isVisible;
