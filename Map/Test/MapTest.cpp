06a290e3cb09e0925197ec33f77d431af69f076e
MikolajOgan <mikolaj.og2002@gmail.com>
2020-02-28 14:01:13 +0100

    Add printing tests

--- Map/Test/MapTest.cpp
+++ Map/Test/MapTest.cpp
@@ -1,5 +1,9 @@
 #include "FieldTypes.hpp"
 #include "SquareMap.hpp"
+#include "EmptyField.hpp"
+#include "FightField.hpp"
+#include "TreasureField.hpp"
+#include "WallField.hpp"
 #include <gtest/gtest.h>
 
 using namespace ::testing;
@@ -32,4 +36,28 @@ TEST_F(SquareMapTest, fieldShouldBeEmpty)
 {
     const auto& field = map.getFields().at(0).at(0);
     ASSERT_EQ(field->getType(), FieldType::Empty);
+}
+
+TEST_F(SquareMapTest, emptyFieldPrint)
+{
+  EmptyField field;
+  ASSERT_EQ('.',map.printField(field));
+}
+
+TEST_F(SquareMapTest, fightFieldPrint)
+{
+  FightField field;
+  ASSERT_EQ('F',map.printField(field));
+}
+
+TEST_F(SquareMapTest, treasureFieldPrint)
+{
+  TreasureField field;
+  ASSERT_EQ('T',map.printField(field));
+}
+
+TEST_F(SquareMapTest, wallFieldPrint)
+{
+  WallField field;
+  ASSERT_EQ('#',map.printField(field));
 }
