89db016a9166dcb8cfe93c73aad51bd0f4368d06
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-02-28 13:09:55 +0100

    Move MapPrinter tests

--- Map/Test/MapTest.cpp
+++ Map/Test/MapTest.cpp
@@ -33,3 +33,24 @@ TEST_F(SquareMapTest, fieldShouldBeEmpty)
     const auto& field = map.getFields().at(0).at(0);
     ASSERT_EQ(field->getType(), FieldType::Empty);
 }
+
+TEST(MapPrintingTest, PrintStandardFieldShouldReturnEmpty)
+{
+    MapPrinter printer;
+    EmptyField emptyfield;
+    ASSERT_EQ(FieldType::Empty, printer.printField(emptyfield));
+}
+
+TEST(MapPrintingTest, PrintFightFieldShouldReturnFight)
+{
+    MapPrinter printer;
+    FightField fightfield;
+    ASSERT_EQ(FieldType::Fight, printer.printField(fightfield));
+}
+
+TEST(MapPrintingTest, PrintTreasureFieldShouldReturnTreasure)
+{
+     MapPrinter printer;
+     TreasureField treasurefield;
+     ASSERT_EQ(FieldType::Treasure, printer.printField(treasurefield));
+}
