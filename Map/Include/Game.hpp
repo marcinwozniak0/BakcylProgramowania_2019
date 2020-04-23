c152db5072e00278af89eb953395048dfdf8cc09
JChoptiany1 <choptianyj@gmail.com>
2020-04-23 15:06:59 +0200

    getDirection() added

--- Map/Include/Game.hpp
+++ Map/Include/Game.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include <ncurses.h>
 #include "Map.hpp"
 
 class Game
 {
 public:
     Game(Map& map) : _map(map) {}
+    Map::Direction getDirection();
 
 private:
     Map& _map;
--- Map/Source/Game.cpp
+++ Map/Source/Game.cpp
@@ -1,1 +1,25 @@
 #include "Game.hpp"
+
+Map::Direction Game::getDirection()
+{
+    initscr();
+    noecho();
+    char direction = getch();
+    switch(direction)
+    {
+        case 'W':
+        case 'w':
+            return Map::Direction::Up;
+        case 'A':
+        case 'a':
+            return Map::Direction::Left;
+        case 'S':
+        case 's':
+            return Map::Direction::Down;
+        case 'D':
+        case 'd':
+            return Map::Direction::Right;
+        default:
+            break;
+    }
+}
