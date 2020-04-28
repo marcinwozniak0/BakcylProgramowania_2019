e1d4f77cc093d8269b200630a65eb1e5cc219b21
JakubDryka <kuba.dryka@wp.pl>
2020-04-28 16:45:18 +0200

    new test for discard deck, now in thest pointers are calling functions

--- Table/Test/TableTests.cpp
+++ Table/Test/TableTests.cpp
@@ -46,26 +46,22 @@ TEST(LimitOfCardsOnTable, ItShouldWarnPlayerAboutTryOfPassingLimitOfCardsOnTable
   ASSERT_EQ(1, handdeck1._cards.size());
 }
 
-// TEST(LimitOfCardsInDiscarddeck, ItShouldWarnPlayerAboutTryOfPassingLimitOfCardsInDiscardDeck)
-// {
-//   Table table1;
-//   Deck pulldeck1;
-//   HandDeck handdeck1(10);
-//
-//   handdeck1.addCard("card1");
-//   handdeck1.addCard("card2");
-//   handdeck1.addCard("card3");
-//   handdeck1.addCard("card4");
-//   handdeck1.addCard("card5");
-//   handdeck1.addCard("card6");
-//
-//   table1.playersTableSite -> playCard(handdeck1, table1._playersTableSite, "card1");
-//   table1.playersTableSite -> playCard(handdeck1, table1._playersTableSite, "card2");
-//   table1.playersTableSite -> playCard(handdeck1, table1._playersTableSite, "card3");
-//   table1.playersTableSite -> playCard(handdeck1, table1._playersTableSite, "card4");
-//   table1.playersTableSite -> playCard(handdeck1, table1._playersTableSite, "card5");
-//   table1.playersTableSite -> playCard(handdeck1, table1._playersTableSite, "card6");
-//
-//   ASSERT_EQ(5, table1._playersTableSite._cards.size());
-//   ASSERT_EQ(1, handdeck1._cards.size());
-// }
+TEST(TransferingCardsToDiscardDeck, ItShouldtransferCardsFromTableDeckToDiscardDeck)
+{
+  Table table1;
+
+  table1.playersTableSite -> addCard("card1");
+  table1.playersTableSite -> addCard("card2");
+  table1.playersTableSite -> addCard("card3");
+  table1.playersTableSite -> addCard("card4");
+  table1.playersTableSite -> addCard("card5");
+
+  table1.playersTableSite -> moveCard(table1._playersDiscardDeck, "card1");
+  table1.playersTableSite -> moveCard(table1._playersDiscardDeck, "card2");
+  table1.playersTableSite -> moveCard(table1._playersDiscardDeck, "card3");
+  table1.playersTableSite -> moveCard(table1._playersDiscardDeck, "card4");
+  table1.playersTableSite -> moveCard(table1._playersDiscardDeck, "card5");
+
+  ASSERT_EQ(5, table1._playersDiscardDeck._cards.size());
+  ASSERT_EQ(0, table1._playersTableSite._cards.size());
+}
