6a1d794a763f9c86e01573eb975f5e65aff52c4a
JakubDryka <kuba.dryka@wp.pl>
2020-04-28 16:50:10 +0200

    refactor code

--- Table/Source/Table.cpp
+++ Table/Source/Table.cpp
@@ -1,8 +1,9 @@
 #include "Table.hpp"
 
  Table::Table()
- :_playersTableSite(5),
-  _computerTableSite(5)
+ :
+ _playersTableSite(5),
+ _computerTableSite(5)
   {
     playersTableSite = std::make_shared<TableDeck>(_playersTableSite);
     computerTableSite = std::make_shared<TableDeck>(_computerTableSite);
