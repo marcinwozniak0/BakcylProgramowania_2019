f4b9c7a162efbedb79bf49502af62a6d874da371
MichalSZtanderaKolanek <mich1239.mks@gmail.com>
2020-05-05 17:12:38 +0200

    prepared CardPool

--- Combat/Include/CardPool.hpp
+++ Combat/Include/CardPool.hpp
@@ -1,12 +1,12 @@
-#pragma once
-#include <vector>
-#include "LimitedDeck.hpp"
-#include "Monster.hpp"
-#include "Spell.hpp"
-
-struct CardPool
-{
-  std::vector<Monster> _monsterCardPool;
-  std::vector<Spell> _SpellCardPool;
-
-};
+// #pragma once
+// #include <vector>
+//#include "LimitedDeck.hpp"
+// #include "Monster.hpp"
+// #include "Spell.hpp"
+//
+// struct CardPool
+// {
+//   std::vector<Monster> _monsterCardPool;
+//   std::vector<Spell> _SpellCardPool;
+//   //TODO zapełnić vectory kartami
+// };
