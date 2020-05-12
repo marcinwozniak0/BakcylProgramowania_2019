1947d9545f2af87741eec3a705d2ff6654dd8330
JChoptiany1 <choptianyj@gmail.com>
2020-05-12 13:59:12 +0200

    getDirection()'s tests fixed

--- Map/Test/GameTest.cpp
+++ Map/Test/GameTest.cpp
@@ -24,40 +24,56 @@ TEST_F(GameTest, isBuildingWell)
 
 TEST_F(GameTest, getDirectionShouldReturnUp)
 {
+    std::istringstream stream("w");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Up, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionShouldReturnDown)
 {
+    std::istringstream stream("s");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Down, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionShouldReturnLeft)
 {
+    std::istringstream stream("a");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Left, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionShouldReturnRight)
 {
+    std::istringstream stream("d");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Right, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionForUppercaseShouldReturnUp)
 {
+    std::istringstream stream("W");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Up, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionForUppercaseShouldReturnDown)
 {
+    std::istringstream stream("S");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Down, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionForUppercaseShouldReturnLeft)
 {
+    std::istringstream stream("A");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Left, game.getDirection());
 }
 
 TEST_F(GameTest, getDirectionForUppercaseShouldReturnRight)
 {
+    std::istringstream stream("D");
+    std::cin.rdbuf(stream.rdbuf());
     ASSERT_EQ(Direction::Right, game.getDirection());
 }
