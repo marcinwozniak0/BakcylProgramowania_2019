b1351783b11f5d15b8c854f3fbbd0dd71ee006aa
Damian Choptiany <damian.choptiany@gmail.com>
2020-05-12 14:18:54 +0200

    Added player to Game class

--- Map/Include/Game.hpp
+++ Map/Include/Game.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #include <ncurses.h>
 #include "Map.hpp"
+#include "Player.hpp"
 
 class Game
 {
 public:
-    Game(Map& map) : _map(map) {}
+    Game(Map& map, Player& player) : _map(map), _player(player) {}
     Direction getDirection();
 
 private:
     Map& _map;
+    Player& _player;
 };
