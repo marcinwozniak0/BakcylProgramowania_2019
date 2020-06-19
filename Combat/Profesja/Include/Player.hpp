c274af3c5e87f2c5c8745de785283ea27c99eefc
JakubDryka <kuba.dryka@wp.pl>
2020-06-19 20:53:08 +0200

    enum for holding profession type inside Player class

--- Combat/Profesja/Include/Player.hpp
+++ Combat/Profesja/Include/Player.hpp
@@ -6,14 +6,18 @@
 
 class Player
 {
+  enum Profession
+  {
+    Default = 0, Mage = 1, Paladin = 2, Rogue = 3,
+  };
 public:
   Points _hp;
   Points _mp;
+  const Profession _ProfType;
   std::vector<std::string> handdeck;
-  Player(Points hp, Points mp, int _xCoordinate = 0, int _yCoordinate = 0);
+  Player(Points hp, Points mp, int _xCoordinate = 0, int _yCoordinate = 0, Profession ProfType = Default);
   const Position& getCurrentPosition();
   void setNewPosition(const Position& targetPosition);
-
   private:
   Position _position;
 };
--- Combat/Profesja/Source/Player.cpp
+++ Combat/Profesja/Source/Player.cpp
@@ -1,9 +1,10 @@
 #include "Player.hpp"
 
-Player::Player(Points hp, Points mp, int _xCoordinate, int _yCoordinate)
-    : _hp(hp), _mp(mp), _position(_xCoordinate, _yCoordinate)
+Player::Player(Points hp, Points mp, int _xCoordinate, int _yCoordinate, Profession ProfType)
+    : _hp(hp), _mp(mp), _ProfType(ProfType), _position(_xCoordinate, _yCoordinate)
 {
   handdeck = {"karta1", "karta2", "karta3"};
+
 }
 
 const Position& Player::getCurrentPosition()
