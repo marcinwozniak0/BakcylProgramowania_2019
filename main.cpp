a6aab6e249b2a0beb67fc688694825e240dc7af5
gizlu <gizlu@gizlu.pl>
2020-08-26 22:23:23 +0200

    Add example map generation in main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
-#include <cstdint>
-#include "Points.hpp"
-#include "Player.hpp"
 #include "LimitedDeck.hpp"
+#include "MapGeneratorV1.hpp"
+#include "Player.hpp"
+#include "Points.hpp"
+#include <cstdint>
+#include <iostream>
 
 int main()
 {
-  /*
-  Fight fight;
-  */
-   Player p1(100, 100, 10, 5, 5);
+    /*
+    Fight fight;
+    */
+    Player p1(100, 100, 10, 5, 5);
 
-   std::cout << "Kaczka!" << std::endl;
-	return 0;
+    auto mapa = MapGeneratorV1::generate(10, 10);
+    mapa.updateVisilibity(Position(1, 1));
+    std::cout << mapa.getMapToPrint(Position(1, 1));
+    std::cout << "Kaczka!" << std::endl;
+    return 0;
 }
